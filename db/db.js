9da6959d73ea4f102cc6249d98289bbcf932567b
hugefiver <hugefiver@outlook.com>
2019-11-23 22:45:08 +0800

    add Database module

---
+++ db/db.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const fspath = require('path');
+
+class DB {
+    constructor(file, timeout=100) {
+        this.data = {};
+        if (!fspath.isAbsolute(file)) {
+            file = fspath.join(fspath.dirname(__filename), file);
+        }
+        this.file = file;
+        this.changed = false;
+        this.timeout = timeout;
+        if (fs.existsSync(file)) {
+            this.data = JSON.parse(fs.readFileSync(file).toString());
+        } else {
+            this.data = {};
+            this.write();
+        }
+        setTimeout(this.daemon.bind(this), this.timeout);
+    }
+
+    daemon() {
+        if (this.changed === true) {
+            this.write();
+            this.changed = false;
+        }
+        setTimeout(this.daemon.bind(this), this.timeout);
+    }
+
+    write() {
+        fs.writeFileSync(this.file, JSON.stringify(this.data, null, 4));
+    }
+}
